2740f56e01e7f1e59a54d63fa3253f385daf9358
SheillaO <sheilla.alusa@strathmore.edu>
2025-07-23 12:16:25 +0300

    Updated the JS

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 function updateTime() {
   // nairobi
   let nairobiElement = document.querySelector("#nairobi");
-  let nairobiDateElement = nairobiElement.querySelector(".date");
-  let nairobiTimeElement = nairobiElement.querySelector(".time");
-  let nairobiTime = moment().tz("Africa/Nairobi");
+  if (nairobiElement) {
+    let nairobiDateElement = nairobiElement.querySelector(".date");
+    let nairobiTimeElement = nairobiElement.querySelector(".time");
+    let nairobiTime = moment().tz("Africa/Nairobi");
 
-  nairobiDateElement.innerHTML = nairobiTime.format("dddd, MMMM Do YYYY");
-  nairobiTimeElement.innerHTML = nairobiTime.format(
-    "h:mm:ss:SS[<small>]A[</small>]"
-  );
+    nairobiDateElement.innerHTML = nairobiTime.format("dddd, MMMM Do YYYY");
+    nairobiTimeElement.innerHTML =
+      nairobiTime.format("h:mm:ss") +
+      " <small>" +
+      nairobiTime.format("A") +
+      "</small>";
+  }
 
   // berlin
   let berlinElement = document.querySelector("#berlin");
-  let berlinDateElement = berlinElement.querySelector(".date");
-  let berlinTimeElement = berlinElement.querySelector(".time");
-  let berlinTime = moment().tz("Europe/Berlin");
+  if (berlinElement) {
+    let berlinDateElement = berlinElement.querySelector(".date");
+    let berlinTimeElement = berlinElement.querySelector(".time");
+    let berlinTime = moment().tz("Europe/Berlin");
+    berlinTimeElement.innerHTML =
+      berlinTime.format("h:mm:ss") +
+      " <small>" +
+      berlinTime.format("A") +
+      "</small>";
+  }
+}
 
-  berlinDateElement.innerHTML = berlinTime.format("dddd, MMMM Do YYYY");
-  berlinTimeElement.innerHTML = berlinTime.format(
-    "h:mm:ss:SS[<small>]A[</small>]"
-  );
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
+  let cityParts = cityTimeZone.split("/");
+  let cityName =
+    cityParts.length > 1
+      ? cityParts[cityParts.length - 1].replace(/_/g, " ")
+      : cityTimeZone;
+
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format(
+      "h:mm:ss"
+    )} <small>${cityTime.format("A")}</small></div>
+  </div>
+  `;
 }
 
 updateTime();
-setInterval(updateTime, 100);
+setInterval(updateTime, 10);
+
+let citiesSelectElement = document.querySelector("#city");
+if (citiesSelectElement) {
+  citiesSelectElement.addEventListener("change", updateCity);
+}
